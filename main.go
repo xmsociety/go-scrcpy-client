35e429b7f6854338ca99982793a6096620e3ae61
sato <ysudqfs@163.com>
2022-08-26 03:06:42 +0800

    add ico

--- main.go
+++ main.go
@@ -3,17 +3,31 @@ package main
 import (
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/driver/desktop"
 	"go-scrcpy-client/ui"
+	"log"
 	"os"
 )
 
 func main() {
 	a := app.NewWithID("go-scrcpy-client")
 	w := a.NewWindow("go-scrcpy-client")
+	a.SetIcon(ui.Logo)
 	ui.MainWindow(w)
 	w.Resize(fyne.Size{Width: 640, Height: 400})
 	w.CenterOnScreen()
+	w.SetIcon(ui.Logo)
+
+	if desk, ok := a.(desktop.App); ok {
+		m := fyne.NewMenu("go-scrcpy-client",
+			fyne.NewMenuItem("Hello", func() {
+				log.Println("Hello")
+			}))
+		desk.SetSystemTrayMenu(m)
+	}
+
 	w.ShowAndRun()
+
 	err := os.Unsetenv("FYNE_FONT")
 	if err != nil {
 		return
